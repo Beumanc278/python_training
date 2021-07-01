dbd2a566cf580e221578aa42d6d7f3e49e13ace5
dmitriy.anurin <anurin.dm.ig@gmail.com>
2021-07-01 09:35:18 +0300

    Добавлено дополнительное создание контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
+from model.contact import Contact
+import pytest
 
-def test_add_contact(app, json_contacts):
-    contact = json_contacts
+testdata = [Contact(first_name=Contact.generate_random_name(10), last_name=Contact.generate_random_name(10),
+                    address=Contact.generate_random_address(15), email1=Contact.generate_random_email(10),
+                    email2=Contact.generate_random_email(10), email3=Contact.generate_random_email(10),
+                    homephone=Contact.generate_random_phone(), workphone=Contact.generate_random_phone(), mobilephone=Contact.generate_random_phone(),
+                    secondaryphone=Contact.generate_random_phone()) for i in range(5)]
+
+@pytest.mark.parametrize('contact', testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
