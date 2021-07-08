57877566fbf39d75d3ad2526621db8d062686209
dmitriy.anurin <anurin.dm.ig@gmail.com>
2021-07-08 19:15:52 +0300

    Обновлены BDD тесты, добавлен тест для удаления группы

--- bdd/group_steps.py
+++ bdd/group_steps.py
@@ -1,11 +1,17 @@
+import random
+
 import pytest
 from pytest_bdd import given, when, then
 from model.group import Group
 
 @pytest.fixture
+def group_list_cache():
+    return []
+
+@pytest.fixture
 @given('a group list')
-def group_list(db):
-    return db.get_group_list()
+def group_list(db, group_list_cache):
+    return group_list_cache.extend(db.get_group_list())
 
 @pytest.fixture
 @given('a group with <name>, <header> and <footer>')
@@ -17,8 +23,38 @@ def add_new_group(app, new_group):
     app.group.create(new_group)
 
 @then('the new group is equal to the old list with the added group')
-def verify_group_added(db, group_list, new_group):
-    old_groups = group_list
+def verify_group_added(db, group_list_cache, new_group):
+    old_groups = group_list_cache
     new_groups = db.get_group_list()
     old_groups.append(new_group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
+@pytest.fixture
+def non_empty_group_list_cache():
+    return []
+
+@pytest.fixture
+@given('a non-empty group list')
+def non_empty_group_list(db, app, non_empty_group_list_cache):
+    if len(db.get_group_list()) == 0:
+        app.group.create(Group(name='some_name'))
+    return non_empty_group_list_cache.extend(db.get_group_list())
+
+@pytest.fixture
+@given('a random group from the list')
+def random_group(non_empty_group_list_cache):
+    return random.choice(non_empty_group_list_cache)
+
+@when("I delete the group from the list")
+def delete_group(app, random_group):
+    app.group.delete_group_by_id(random_group.id)
+
+@then('the new group list is equal to the old list without the deleted group')
+def verify_group_deleted(app, db, check_ui, non_empty_group_list_cache, random_group):
+    old_groups = non_empty_group_list_cache
+    new_groups = db.get_group_list()
+    assert len(old_groups) - 1 == len(new_groups)
+    old_groups.remove(random_group)
+    assert old_groups == new_groups
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
